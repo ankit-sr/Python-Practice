cb0cfdf9b8c388b63e8bc4f716705321f7edcf3d
ankit-sr <ankitrajput6622@gmail.com>
2021-10-06 19:51:30 +0530

    first commit

---
+++ while_loop.py
@@ -0,0 +1,14 @@
+'''
+    while loops are used to iterate over a particular based on the correctness of the condition provided
+    Syntax : while condition:
+                Code
+    So, as long as the condition holds true, the loop continues iterating.
+'''
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
+
+print('\ni = ' + str(i) + '; done printing loop')
