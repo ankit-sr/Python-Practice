9a951f3893c694ef9ce495f7fea5bd048dc47f01
ankit-sr <ankitrajput6622@gmail.com>
2021-10-06 14:58:26 +0530

    first commit

---
+++ tuple.py
@@ -0,0 +1,13 @@
+''' Like lists, tuples are a structure in python than can contain more than one element of
+    diferent data types. But, tuples are immutable, that means a tuple value can only be assigned once
+    and remains, intact during the execution of program.
+
+    That means, that we can only access (read-only mode) the elements of the tuple, but can not change it
+    on our convenience.
+'''
+
+names = ('ankit', 'prateek', 'sushant')
+
+# accessing elements of tuples using their indexes
+print(names[0] + names[1] + names[3])
+
