fa097a1a7bb810c2e3054f8b66a4a3ad65d2dd2e
ankit-sr <ankitrajput6622@gmail.com>
2021-10-07 15:30:03 +0530

    first commit

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+'''
+    Dictionary is again a structure in python which comprises of key-value pairs.
+    A typically dictionary holds some word(or element) called as key and its meaning 
+    called as value.
+    A key can be of any immutable data-type such as int, float, string, boolean, but should not be
+    repeated within a dictionary,
+    while a value can be anything 
+'''
+
+person = {
+    'name': 'Prateek',
+    'age': 21,
+    'course': 'Python',
+    'interest': 'buses'
+}
+
+print(person)
