5ad7c9cb96613b222adb5f380b0dbff6fa31baba
ankit-sr <ankitrajput6622@gmail.com>
2021-10-09 16:06:33 +0530

    first commit

---
+++ student.py
@@ -0,0 +1,8 @@
+class Student :
+
+    def __init__(self, name, age, subject='Science', percent=80.0, on_leave=False):
+        self.name = name
+        self.age = age
+        self.subject = subject
+        self.percent = percent
+        self.on_leave = on_leave
