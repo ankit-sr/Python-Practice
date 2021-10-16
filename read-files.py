d1743076da24e3c42beedcf9d6f1b988fba98f2e
ankit-sr <ankitrajput6622@gmail.com>
2021-10-17 01:14:54 +0530

    appended modes of opening file

--- read-files.py
+++ read-files.py
@@ -1,14 +1,19 @@
 '''
     We can manage files in python.
-    A file can be opened in 4 modes : r - read,
-    w - write, a - append and r+ - read and write
+    A file can be opened in 6 modes : 
+    r - only read,
+    w - write: destroys the content of the file if already exists and starts as new, 
+    a - append: allows to append the data at the ending of the file and 
+    r+ - read and write
+    w+ - write and read : destroys the content, if file exists
+    a+ - append and read: cursor points at the end of file.
 
     while managing files, keep in mind to open the correct file and
     also close the file.
 '''
 
-'''
-    we should not use read() with readlines() because, when either of the function is called
+''' NOTE:
+    We should not use read() with readlines() because, when either of the function is called
     the cursor in the file, moves to the end of file (EoF), therefore either one of them will be executed
     whichever is written first.
 '''
